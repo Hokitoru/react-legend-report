aadcf5bc26c4ada8acf35028a05d56057c4061dd
Hokitoru <vol4ara202@list.ru>
2022-12-17 18:17:46 +0300

    Auth

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Auth from "./components/Auth/Auth";
 import {useSelector} from "react-redux";
+import {useState} from "react";
 
 function App() {
     let userName = useSelector(state => state.name);
+    const [userNameSwitcher, setUserNameSwitcher] = useState(true);
 
     return (
         <div className="App">
             <Header></Header>
             {
-                userName === 'noname' ? <Auth></Auth> : ''
+                userName === 'noname' && userNameSwitcher === true ? <Auth userNameSwitcher={userNameSwitcher} setUserNameSwitcher={setUserNameSwitcher()}></Auth> : <input type="text"/>
             }
         </div>
     );
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,17 +3,24 @@ import classes from './style.module.scss'
 import {useDispatch} from "react-redux";
 import {changeNameAction} from "../Store/nameReducer";
 
-const Auth = () => {
+const Auth = ({userNameSwitcher, setUserNameSwitcher}) => {
     const dispatch = useDispatch();
+    const [name, setName] = useState(null);
 
-    const changeName = (name) => {
-        dispatch(changeNameAction(name));
+    const changeName = (event) => {
+        if(event.key === 'Enter' || event.type === 'click'){
+            dispatch(changeNameAction(name));
+        }
     }
 
     return (
         <div className={classes.container}>
             <h2>Добрый день, как к вам обращаться?</h2>
-            <input type="text" onChange={event => changeName(event.target.value, event.)}/>
+            <input type="text" onChange={event => setName(event.target.value)} onKeyUp={event => changeName(event)}/>
+            <div>
+                <button onClick={setUserNameSwitcher(!userNameSwitcher)}>Пропустить</button>
+                <button onClick={event => changeName(event)}>Готово</button>
+            </div>
         </div>
     );
 };
